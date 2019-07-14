c980419c81bd6f8d9998ca364e0ce50d2ff644d0
awen <awen@163.com>
2019-07-14 13:23:37 +0800

    '项目初始化提交'

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react'
+import { directive } from '@babel/types';
+
+export default class App extends Component{
+    render(){
+        return(
+            <div>项目开始</div>
+        )
+    }
+}
