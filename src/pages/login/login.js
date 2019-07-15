5a8cc230d75c37ba0e61b6522c18df08c15fec3e
awen <awen@163.com>
2019-07-15 15:08:29 +0800

    '表单验证'

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,13 +1,29 @@
 import React,{Component} from 'react'
-import { Form, Icon, Input, Button} from 'antd';
+import { Form, Icon, Input, Button,message} from 'antd';
 import login from './images/logo.png'
 import './login.less'
-export default class Login extends Component{
+// import { min } from 'moment';
+class Login extends Component{
     handleSubmit = e => {
         e.preventDefault();
-        alert('发送Ajax请求')
+        message.success('发送Ajax请求')
+    }
+    validatepwd = (rules,value,callback)=>{
+        value = value.trim();
+        if(!value){
+            callback('密码必须输入')
+        }else if(value.length<4){
+            callback('密码不能小于4位')
+        }else if(value.length>4){
+            callback('密码不能大于12位')
+        }else if(!/^[a-zA-Z0-9_]+$/.test(value)){
+            callback('密码必须是英文、数字或下划线组成')
+        }else{
+            callback()
+        }
     }
     render(){
+        const {getFieldDecorator} = this.props.form
         return(
             <div className="login">
                 <div className="login-header">
@@ -18,22 +34,47 @@ export default class Login extends Component{
                     <h1>用户登录</h1>
                     <Form onSubmit={this.handleSubmit} className="login-form">
                         <Form.Item>
-                        <Input
-                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                            placeholder="用户名"
-                        />
+                            {
+                                getFieldDecorator('username',{
+                                    //配置对象：属性名是一些特定的名称
+                                    initialValue:'',//初始值
+                                    rules:[
+                                        //声明式验证
+                                        {required:true,whitespace:true,message:'用户名是必须'},
+                                        {min:4,message:'用户名不能小于4位'},
+                                        {max:12,message:'用户名不能大于12位'},
+                                        {pattern:/^[a-zA-Z0-9_]+$/,message:'用户名必须是英文、数字或下划线组成'}
+                                    ]
+                                })(
+                                    <Input
+                                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                        placeholder="用户名"
+                                    />
+                                )
+                            }
+                            
                         </Form.Item>
                         <Form.Item>
-                        <Input
-                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                            type="password"
-                            placeholder="密码"
-                        />
+                            {
+                                getFieldDecorator('password',{
+                                    initialValue:'',
+                                    rules:[
+                                        {validator:this.validatepwd}
+                                    ]
+                                })(
+                                    <Input
+                                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                        type="password"
+                                        placeholder="密码"
+                                    />
+                                )
+                            }
+                            
                         </Form.Item>
                         <Form.Item>
-                        <Button type="primary" htmlType="submit" className="login-form-button">
-                            Log in
-                        </Button>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                Log in
+                            </Button>
                         </Form.Item>
                     </Form>
                 </div>
@@ -41,3 +82,5 @@ export default class Login extends Component{
         )
     }
 }
+const WrapperForm = Form.create()(Login)
+export default WrapperForm
