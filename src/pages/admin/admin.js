d1935cd204bdaae3c333bad9c54790a521212836
awen <awen@163.com>
2019-07-16 12:59:05 +0800

    'Admin页面半成品'

--- src/pages/admin/admin.js
+++ src/pages/admin/admin.js
@@ -1,6 +1,21 @@
 import React,{Component} from 'react'
-import {Redirect} from 'react-router-dom'
+import {Redirect,Route} from 'react-router-dom'
+import {Layout, Switch} from 'antd'
+
 import memoryUtils from '../../utils/memoryUtils'
+
+import LeftNav from '../../components/left-nav/index'
+import Header from '../../components/header/index'
+import Home from '../home/home'
+import Category from '../category/category'
+import Product from '../product/product'
+import Role from '../role/role'
+import User from '../user/user'
+import Bar from '../charts/bar'
+import Line from '../charts/line'
+import Pie from '../charts/pie'
+
+const {Footer,Sider,Content} = Layout
 export default class Admin extends Component{
     render(){
         const user = memoryUtils.user
@@ -8,9 +23,31 @@ export default class Admin extends Component{
             return <Redirect to='/login'/>
         }
         return(
-            <div>
-                hello,{user.username}
-            </div>
+            <Layout style={{width:'100%'}}>
+                <Sider>
+                    <LeftNav/>
+                </Sider>
+                <Layout>
+                    <Header/>
+                    <Content style={{background:'red'}}>
+                        <Switch>
+                            <Route path="/home" component={Home}/>
+                            <Route path='/category' component={Category} />
+                            <Route path='/product' component={Product} />
+                            <Route path='/role' component={Role} />
+                            <Route path='/user' component={User} />
+                            <Route path='/charts/bar' component={Bar} />
+                            <Route path='/charts/line' component={Line} />
+                            <Route path='/charts/pie' component={Pie} />
+                            <Redirect to="/home"/>
+                        </Switch>
+                    </Content>
+                    
+                </Layout>
+                <Footer>
+                    
+                </Footer>
+            </Layout>
         )
     }
 }
